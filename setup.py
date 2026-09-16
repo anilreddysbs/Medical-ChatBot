500af218f2f7efa938a31e3f20e7d77f3fd3ec47
Anil Reddy <anilreddysbs@gmail.com>
2026-09-16 09:33:08 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="Medical_ChatBot",
+    version="0.1.0",
+    author="Anil",
+    author_email="anilreddysbs@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
